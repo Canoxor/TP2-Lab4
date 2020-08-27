f056badbd91f5deaae42aa1478e0b8c7f988837a
Canoxor <54516362+Canoxor@users.noreply.github.com>
2020-08-27 12:57:40 -0300

    TP2 - Punto 1 y 2

---
+++ TP2/TP-2/src/paquete/Persona.java
@@ -0,0 +1,22 @@
+package paquete;
+
+public class Persona {
+
+	private int dni;
+	private String nombre;
+	private String apellido;
+	
+	public void Persona()
+	{
+		dni = 11111111;
+		nombre = "";
+		apellido = "";
+	}
+	
+	public void Persona(int dni, String nombre, String apellido)
+	{
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+}
